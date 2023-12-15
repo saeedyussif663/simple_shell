662335849b89701ff888fa553d2e447f2b9af3cd
saeedyussif663 <saeedyussif663@gmail.com>
2023-12-15 03:31:35 -0800

    vars 2func

--- vars.c
+++ vars.c
@@ -37,25 +37,23 @@ return (1);
 
 
 /**
- * check_chain - checks we should continue chaining based on last status
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
- * @i: starting position in buf
- * @len: length of buf
- *
+ * check_chain - checks chaining?
+ * @info: parameter
+ * @buf: parameter
+ * @p: parameter
+ * @i: parameter
+ * @len: parameter
  * Return: Void
  */
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
-size_t j = *p;
+size_t a = *p;
 
 if (info->cmd_buf_type == CMD_AND)
 {
 if (info->status)
 {
-buf[i] = 0;
-j = len;
+buf[i] = 0;a = len;
 }
 }
 if (info->cmd_buf_type == CMD_OR)
@@ -63,12 +61,13 @@ if (info->cmd_buf_type == CMD_OR)
 if (!info->status)
 {
 buf[i] = 0;
-j = len;
+a = len;
 }
 }
 
-*p = j;
+*p = a;
 }
+
 
 /**
  * replace_alias - replaces an aliases in the tokenized string
